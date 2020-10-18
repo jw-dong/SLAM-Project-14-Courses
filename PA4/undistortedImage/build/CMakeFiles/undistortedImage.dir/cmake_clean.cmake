69951011a5bc9ded22685e9e42ae81a2f1a7d1cf
Jiawei Dong <stephen_dong_127@hotmail.com>
2020-10-17 23:56:01 -0500

    adding homework PA4

---
+++ PA4/undistortedImage/build/CMakeFiles/undistortedImage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/undistortedImage.dir/undistort_image.cpp.o"
+  "undistortedImage"
+  "undistortedImage.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/undistortedImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

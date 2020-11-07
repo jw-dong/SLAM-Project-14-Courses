e9825ccc491ab97252f741d1005c4bb48fa18e69
Jiawei Dong <stephen_dong_127@hotmail.com>
2020-11-07 17:14:52 -0600

    homework 7

---
+++ PA7/BA direct/build/CMakeFiles/ba_direct.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ba_direct.dir/directBA.cpp.o"
+  "ba_direct"
+  "ba_direct.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ba_direct.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ PA7/BA/build/CMakeFiles/ba_g2o.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ba_g2o.dir/ba_g2o.cpp.o"
+  "ba_g2o"
+  "ba_g2o.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ba_g2o.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
